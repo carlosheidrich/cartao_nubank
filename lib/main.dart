ea8bb3833212bdb0755a68ed61b98fc7a26c5680
Carlos Alexandre Heidrich <74748328+carlosheidrich@users.noreply.github.com>
2020-12-06 01:13:36 -0300

    Refatoração com TextField

--- lib/main.dart
+++ lib/main.dart
@@ -1,155 +1,43 @@
 import 'dart:ui';
+import 'package:brasil_fields/brasil_fields.dart';
 import 'package:flutter/material.dart';
 import 'package:flip_card/flip_card.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(NubankPage());
 }
 
-bool frente = true;
+class NubankPage extends StatefulWidget {
+  @override
+  _NubankPageState createState() => _NubankPageState();
+}
 
-class MyApp extends StatelessWidget {
+class _NubankPageState extends State<NubankPage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: NubankFrente(),
-    );
-  }
-}
-
-class NubankFrente extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(153, 51, 153, 0.5),
-      appBar: AppBar(
-          backgroundColor: Color.fromRGBO(109, 33, 119, 1),
-          title: Text('Cartão do Nubank'),
-          actions: <Widget>[
-            InkWell(
-              onTap: () {
-                if (frente) {
-                  frente = false;
-                  NubankVerso();
-                } else {
-                  frente = true;
-                  NubankFrente();
-                }
-              },
-              child: Icon(
+      home: Scaffold(
+        backgroundColor: Color.fromRGBO(60, 25, 79, 1),
+        appBar: AppBar(
+            backgroundColor: Color.fromRGBO(109, 33, 119, 1),
+            title: Text('Cartão do Nubank'),
+            actions: <Widget>[
+              Icon(
                 Icons.credit_card_outlined,
                 size: 30,
               ),
-            ),
-            SizedBox(
-              width: 20,
-            ),
-          ]),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10),
-        child: Center(
-          child: AspectRatio(
-            aspectRatio: 8.5 / 5.4,
+              SizedBox(
+                width: 20,
+              ),
+            ]),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Center(
             child: FlipCard(
-              direction: FlipDirection.VERTICAL, // default
-              front: Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(97, 47, 116, 1),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Stack(
-                  children: [
-                    Positioned(
-                      right: 10,
-                      top: 10,
-                      child: Image.asset(
-                        'assets/mastercard.png',
-                        width: 90,
-                      ),
-                    ),
-                    Positioned(
-                      left: 40,
-                      top: 90,
-                      child: Image.asset(
-                        'assets/chip.png',
-                        width: 60,
-                      ),
-                    ),
-                    Positioned(
-                      left: 110,
-                      top: 100,
-                      child: Image.asset(
-                        'assets/nfc.png',
-                        width: 30,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Positioned(
-                      left: 20,
-                      top: 130,
-                      child: Image.asset(
-                        'assets/nu_logo.png',
-                        width: 120,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Positioned(
-                      left: 150,
-                      top: 190,
-                      child: Text(
-                        'Carlos A Heidrich',
-                        style: TextStyle(
-                          fontSize: 22,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              back: Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(97, 47, 116, 1),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Stack(
-                  children: [
-                    Positioned(
-                      top: 20,
-                      child: Container(
-                        width: 400,
-                        height: 80,
-                        color: Colors.white70,
-                      ),
-                    ),
-                    Positioned(
-                      right: 15,
-                      bottom: 15,
-                      child: Image.asset(
-                        'assets/cirrus.png',
-                        width: 80,
-                      ),
-                    ),
-                    Positioned(
-                      left: 40,
-                      top: 190,
-                      child: Text(
-                        '9999 9999 9999 9996',
-                        style: TextStyle(
-                          fontSize: 22,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              direction: FlipDirection.HORIZONTAL,
+              front: NubankFrente(),
+              back: NubankVerso(),
             ),
           ),
         ),
@@ -158,80 +46,217 @@ class NubankFrente extends StatelessWidget {
   }
 }
 
-class NubankVerso extends StatelessWidget {
+class NubankFrente extends StatefulWidget {
+  @override
+  _NubankFrenteState createState() => _NubankFrenteState();
+}
+
+class _NubankFrenteState extends State<NubankFrente> {
+  String nome = '';
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(153, 51, 153, 0.5),
-      appBar: AppBar(
-          backgroundColor: Color.fromRGBO(109, 33, 119, 1),
-          title: Text('Cartão do Nubank'),
-          actions: <Widget>[
-            InkWell(
-              onTap: () {
-                if (frente) {
-                  frente = false;
-                  NubankVerso();
-                } else {
-                  frente = true;
-                  NubankFrente();
-                }
-              },
-              child: Icon(
-                Icons.credit_card_outlined,
-                size: 30,
+    return Stack(
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5),
+              child: Center(
+                child: AspectRatio(
+                  aspectRatio: 8.5 / 5.4,
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(97, 47, 116, 1),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          right: 10,
+                          top: 10,
+                          child: Image.asset(
+                            'assets/mastercard.png',
+                            width: 90,
+                          ),
+                        ),
+                        Positioned(
+                          left: 40,
+                          top: 70,
+                          child: Image.asset(
+                            'assets/chip.png',
+                            width: 60,
+                          ),
+                        ),
+                        Positioned(
+                          left: 110,
+                          top: 80,
+                          child: Image.asset(
+                            'assets/nfc.png',
+                            width: 30,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Positioned(
+                          left: 20,
+                          top: 105,
+                          child: Image.asset(
+                            'assets/nu_logo.png',
+                            width: 120,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Positioned(
+                          left: 150,
+                          top: 165,
+                          child: Text(
+                            '$nome',
+                            style: TextStyle(
+                              fontSize: 22,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ),
             SizedBox(
-              width: 20,
+              height: 30,
             ),
-          ]),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10),
-        child: Center(
-          child: AspectRatio(
-            aspectRatio: 8.5 / 5.4,
-            child: Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(97, 47, 116, 1),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Stack(
-                children: [
-                  Positioned(
-                    top: 20,
-                    child: Container(
-                      width: 400,
-                      height: 80,
-                      color: Colors.white70,
-                    ),
+            Container(
+              height: 70,
+              child: TextField(
+                textInputAction: TextInputAction.send,
+                keyboardType: TextInputType.text,
+                textCapitalization: TextCapitalization.words,
+                maxLength: 30,
+                maxLengthEnforced: true,
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    gapPadding: 50,
+                    borderRadius: BorderRadius.circular(50),
                   ),
-                  Positioned(
-                    right: 15,
-                    bottom: 15,
-                    child: Image.asset(
-                      'assets/cirrus.png',
-                      width: 80,
-                    ),
-                  ),
-                  Positioned(
-                    left: 40,
-                    top: 190,
-                    child: Text(
-                      '9999 9999 9999 9999',
-                      style: TextStyle(
-                        fontSize: 22,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ],
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+                onChanged: (value) {
+                  print(value);
+                  setState(() {
+                    nome = value;
+                  });
+                },
               ),
             ),
-          ),
+          ],
         ),
-      ),
+      ],
+    );
+  }
+}
+
+class NubankVerso extends StatefulWidget {
+  @override
+  _NubankVersoState createState() => _NubankVersoState();
+}
+
+class _NubankVersoState extends State<NubankVerso> {
+  String cartao = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Center(
+                child: AspectRatio(
+                  aspectRatio: 8.5 / 5.4,
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(97, 47, 116, 1),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          top: 20,
+                          child: Container(
+                            height: 80,
+                            width: 400,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        Positioned(
+                          right: 15,
+                          bottom: 15,
+                          child: Image.asset(
+                            'assets/cirrus.png',
+                            width: 80,
+                          ),
+                        ),
+                        Positioned(
+                          left: 15,
+                          bottom: 20,
+                          child: Text(
+                            '$cartao',
+                            style: TextStyle(
+                              fontSize: 22,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 50,
+              child: TextField(
+                textInputAction: TextInputAction.send,
+                keyboardType: TextInputType.number,
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                  CartaoBancarioInputFormatter(),
+                ],
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    gapPadding: 50,
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+                onChanged: (value) {
+                  print(value);
+                  setState(() {
+                    cartao = value;
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
